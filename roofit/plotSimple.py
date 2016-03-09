78755aa0332193d979cf37d30ee32a0b76d88680
YoungKwonJo <cccpy98@gmail.com>
2016-03-09 17:15:46 +0100

    add it

---
+++ roofit/plotSimple.py
@@ -0,0 +1,46 @@
+from ROOT import *
+
+gStyle.SetOptStat(0)
+
+import sys
+if len(sys.argv) < 3:
+  sys.exit()
+
+a1 = int(sys.argv[1]) # 
+a2 = int(sys.argv[2]) # 
+a3 = int(sys.argv[3]) # 
+
+
+f = None
+fs = ["ttbar","ttcc"]
+if a1<2 : f = TFile.Open(fs[a1]+".root")
+else    : sys.exit()
+
+folder = None
+folders = ["NJets20","NbJets20","NcJets20","NaddcJets20","NaddbJets20"]
+if a2<5: folder= folders[a2]
+
+ch=""
+chs=["SL","LL","HH"]
+if a1 is 0 and a3<3: ch=chs[a3]
+
+
+wi = folder+"/h"+folder+ch+"withCW"
+wo = folder+"/h"+folder+ch+"withoutCW"
+
+h  =  f.Get(wi).Clone("ttbar")
+h2 =  f.Get(wo).Clone("ttbar2")
+h2.GetXaxis().SetTitle(folder)
+h.SetLineColor(2)
+h2.SetLineColor(4)
+
+h.Scale( 1./h.Integral() ), h2.Scale( 1./h2.Integral() )
+
+h2.SetMaximum( h2.GetMaximum()*1.5 )
+h2.Draw(), h.Draw("same")
+
+leg = TLegend(0.55,0.8, 0.89,0.89)
+leg.AddEntry(h,"with cq from W in "+ch+" for "+fs[a1] )
+leg.AddEntry(h2,"without cq from W in "+ch+" for "+fs[a1] )
+leg.Draw()
+
