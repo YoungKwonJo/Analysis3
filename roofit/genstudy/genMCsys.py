eafee05e1491e006087f0050061a8a4e6330c861
YoungKwonJo <cccpy98@gmail.com>
2016-05-10 16:40:57 +0200

    update it

--- roofit/genstudy/genMCsys.py
+++ roofit/genstudy/genMCsys.py
@@ -20,33 +20,6 @@ def main():
   PrintSys(POW2,POHP2)
 
 
-  print ""
-  print "Down"
-  dwPOWnom=printV(dwPOW['nom']['dwPOW'],False)
-  dwPOWnomSys=PrintSys(POW2,dwPOWnom)
-  dwPOWmax=compareSYS(dwPOWnomSys,dwPOWnomSys)
-  for i in range(1,4):
-    dwPOW3=printV(dwPOW['Q2_Dw3']['dwPOW'],False)
-    dwPOWsys=PrintSys(POW2,dwPOW3)
-    dwPOWmax=compareSYS(dwPOWmax,dwPOWsys)
-
-  print dwPOWmax
-
-  print ""
-  print "Up"
-  upPOWnom=printV(upPOW['nom']['upPOW'],False)
-  upPOWnomSys=PrintSys(POW2,upPOWnom)
-  upPOWmax=compareSYS(upPOWnomSys,upPOWnomSys)
-  for i in range(1,4):
-    upPOW3=printV(upPOW['Q2_Up3']['upPOW'],False)
-    upPOWsys=PrintSys(POW2,upPOW3)
-    upPOWmax=compareSYS(upPOWmax,upPOWsys)
-
-  print upPOWmax
-
-  print "total:"
-  print compareSYS(upPOWmax,dwPOWmax)
-
 if __name__ == "__main__":
     test=main()
 
