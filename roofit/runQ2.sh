4a25cd8206c2e31fcaf10985a6c8af247d76c1ab
YoungKwonJo <cccpy98@gmail.com>
2016-06-13 13:26:41 +0200

    update it

--- roofit/runQ2.sh
+++ roofit/runQ2.sh
@@ -3,6 +3,9 @@
 #for i in {0..24}
 for i in {0..2}
 do
-   nohup python genTtBbStudy.py $i -b >& Q2_Weights$i.log & 
+  for j in {0..6}
+  do
+    nohup python genTtBbStudy.py $i $j -b >& Q2_Weights$i_$j.log & 
+  done
 done
 
