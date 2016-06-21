dd6bdaeb4dcb49f8e0464c24301a2bda0e470a72
YoungKwonJo <cccpy98@gmail.com>
2016-06-21 08:43:44 +0200

    udpate it

---
+++ roofit/genstudy2/q2Weight_Run.py
@@ -0,0 +1,192 @@
+from q2Weight2 import POW,dwPOW,upPOW,AMC,MG5,POHP
+from ROOT import TH1D
+
+def getError(num,denum):
+  h1 = TH1D("h1","",1,0,10)
+  h2 = TH1D("h2","",1,0,10)
+  h1.Sumw2()
+  h2.Sumw2()
+#ttbbF': 18594.0, 'ttbb': 8194.0,ttbb': 2236.0,
+  for i in range(int(num)):
+    h1.Fill(1)
+  for i in range(int(denum)):
+    h2.Fill(1)
+  h1.Divide(h2)
+  #h1.Draw()
+  #print h1.GetBinContent(1)
+  #print h1.GetBinError(1)
+  return h1.GetBinError(1)
+
+def getNumbers(aaa,key,key2):
+  ttbbFS = aaa["S0"]["ttbbF"]
+  ttjjFS = aaa["S0"]["ttbbF"]+aaa["S0"]["ttbF"]+aaa["S0"]["tt2bF"]+aaa["S0"]["ttccF"]+aaa["S0"]["ttlfF"]
+  ttbbVS = aaa["S0"]["ttbb"]
+  ttjjVS = aaa["S0"]["ttbb"]+aaa["S0"]["ttb"]+aaa["S0"]["tt2b"]+aaa["S0"]["ttcc"]+aaa["S0"]["ttlf"]
+
+  ttbbS6 = aaa["S6"]["ttbb"]
+  ttjjS6 = aaa["S6"]["ttbb"]+aaa["S6"]["ttb"]+aaa["S6"]["tt2b"]+aaa["S6"]["ttcc"]+aaa["S6"]["ttlf"]
+
+  total=aaa["dileptonicP1"]+aaa["dileptonicP1not"]
+  llratio = aaa["dileptonicP1"]/total
+
+  ttbarCX=831.76
+  ttjjFSCX = ttbarCX/llratio*ttjjFS/total
+  ttbbFSCX = ttbarCX/llratio*ttbbFS/total
+  ttjjVSCX = ttbarCX/llratio*ttjjVS/total
+  ttbbVSCX = ttbarCX/llratio*ttbbVS/total
+
+  output = {"key":key,"key2":key2,"ttbbFS": ttbbFS, "ttjjFS":ttjjFS, "ttbbVS":ttbbVS, "ttjjVS":ttjjVS, "llratio":llratio,"ttjjAcc": ttjjVS/ttjjFS*llratio, "ttbbAcc":ttbbVS/ttbbFS*llratio,"total":total, "ttbbS6":ttbbS6, "ttjjS6":ttjjS6, "ttbbEff":ttbbS6/ttbbVS, "ttjjEff":ttjjS6/ttjjVS, "ttjjFSCX":ttjjFSCX,"ttbbFSCX":ttbbFSCX,"ttjjVSCX":ttjjVSCX,"ttbbVSCX":ttbbVSCX }
+  return output
+  #return ttbbFS/llratio,ttjjFS/llratio,ttbbVS,ttjjVS,llratio
+
+def roudV(val1):
+  val =abs(val1)
+  if val > 100 :
+    return int(round(val))
+  elif val > 10 :
+    return int(round(val))
+  elif val > 1 :
+    return round(val*10)/10
+  else :
+    nom=10.
+    if val>0.1    : nom=100.
+    elif val>0.01 : nom=1000.
+    else          : nom=10000.
+#    elif val>0.001: nom=1000000.
+#    elif val>0.0001: nom=10000000.
+#    else          : nom=1000000000.
+
+    return round(val*nom)/nom
+
+def printARow(bbb):
+  print bbb["key"]+" & "+str(roudV(bbb["ttjjAcc"]))+" & "+str(roudV(bbb["ttbbAcc"]))+" \\\\ "
+
+def getSys(a,b):
+  return abs(1/a-1/b)/(1/a)
+  #return abs(a-b)/a
+
+def printARow2(bbb,aaa):
+  ttjjAccSys=getSys(aaa["ttjjAcc"],bbb["ttjjAcc"])
+  ttbbAccSys=getSys(aaa["ttbbAcc"],bbb["ttbbAcc"])
+
+  ttjjEffSys=getSys(aaa["ttjjEff"],bbb["ttjjEff"])
+  ttbbEffSys=getSys(aaa["ttbbEff"],bbb["ttbbEff"])
+
+  MEs = {}
+  MEs["nom"] = "muR=1 muF=1"
+  MEs["weight"] = "muR=1 muF=1"
+  MEs["Q2_Dw1"] = "muR=1 muF=0.5"
+  MEs["Q2_Dw2"] = "muR=0.5 muF=1"
+  MEs["Q2_Dw3"] = "muR=0.5 muF=0.5"
+  MEs["Q2_Up1"] = "muR=1 muF=2"
+  MEs["Q2_Up2"] = "muR=2 muF=1"
+  MEs["Q2_Up3"] = "muR=2 muF=2"
+  tot={}
+  tot["POW"] =97994442.0
+  tot["dwPOW"]=9932876.0
+  tot["upPOW"]=9919776.0
+  tot["AMC"] =12771412.0
+  tot["MG5"] =10215131.0
+  tot["POHP"]=19383463.0 
+
+  
+  label={}
+  label["POW"] ="POWHEG"
+  label["dwPOW"]="$PS_{dw}$"
+  label["upPOW"]="$PS_{up}$"
+  label["AMC"] ="aMC@NLO"
+  label["MG5"] ="Madgraph"
+  label["POHP"]="POW\\_H++"
+  
+
+  total1 = tot[bbb["key2"]]
+  total2 = bbb["total"]
+  totalCorr = total1/total2
+
+  from math import sqrt
+  ttjjAcc = bbb["ttjjAcc"]
+  ttbbAcc = bbb["ttbbAcc"]
+  ttjjEff = bbb["ttjjEff"]
+  ttbbEff = bbb["ttbbEff"]
+
+  ttjjFS = bbb["ttjjFS"]
+  ttjjVS = bbb["ttjjVS"]
+  ttbbFS = bbb["ttbbFS"]
+  ttbbVS = bbb["ttbbVS"]
+  ttjjS6 = bbb["ttjjS6"]
+  ttbbS6 = bbb["ttbbS6"]
+
+  llratio = bbb["llratio"] #sqrt(total1)/sqrt(bbb["llratio"]*total1)*sqrt(bbb["llratio"])
+  statAcc_ttjj = getError(ttjjVS*totalCorr, ttjjFS*totalCorr)#*statAcc2
+  statAcc_ttbb = getError(ttbbVS*totalCorr, ttbbFS*totalCorr)#*statAcc2
+  
+  statEff_ttjj = getError(ttjjS6*totalCorr, ttjjVS*totalCorr)
+  statEff_ttbb = getError(ttbbS6*totalCorr, ttbbVS*totalCorr)
+
+
+  """
+  PSs = {}
+  PSs["POW"] = "POW "
+  PSs["dwPOW"]="$PS_{dw}$ "
+  PSs["upPOW"]="$PS_{up}$ "
+  """
+  bbb["statAcc_ttjj"]=statAcc_ttjj/ttjjAcc
+  bbb["statAcc_ttbb"]=statAcc_ttbb/ttbbAcc
+  bbb["statEff_ttjj"]=statEff_ttjj/ttjjEff
+  bbb["statEff_ttbb"]=statEff_ttbb/ttbbEff
+
+  bbb["sysAcc_ttjj"]=ttjjAccSys
+  bbb["sysAcc_ttbb"]=ttbbAccSys
+  bbb["sysEff_ttjj"]=ttjjEffSys
+  bbb["sysEff_ttbb"]=ttbbEffSys
+
+  nn=40
+  ccc= label[bbb["key2"]].ljust(10)
+  ccc+=(" & "+MEs[bbb["key"]]).ljust(25)
+  ccc+=(" & "+str(roudV(ttjjAcc))+" $\pm$ ("+str(roudV(statAcc_ttjj*100./ttjjAcc))+" \\%) $\pm$ ("+str(roudV(ttjjAccSys*100.))+" \\%) ").ljust(nn)
+  ccc+=(" & "+str(roudV(ttbbAcc))+" $\pm$ ("+str(roudV(statAcc_ttbb*100./ttbbAcc))+" \\%) $\pm$ ("+str(roudV(ttbbAccSys*100.))+" \\%) ").ljust(nn)
+  ccc+=(" & "+str(roudV(ttjjEff))+" $\pm$ ("+str(roudV(statEff_ttjj*100./ttjjEff))+" \\%) $\pm$ ("+str(roudV(ttjjEffSys*100.))+" \\%) ").ljust(nn)
+  ccc+=(" & "+str(roudV(ttbbEff))+" $\pm$ ("+str(roudV(statEff_ttbb*100./ttbbEff))+" \\%) $\pm$ ("+str(roudV(ttbbEffSys*100.))+" \\%) ").ljust(nn)
+  #ccc+=(" & ")+str(bbb["total"] ).ljust(20)
+  ccc+=" \\\\ "
+  print ccc
+
+def getOutputAll(aaa):
+  outputAll = {}
+  for key in aaa.keys():
+    key2=aaa[key][key].keys()[0]
+    bbb=aaa[key][key][key2]
+    outputAll[key]=getNumbers(bbb,key,key2)
+  return outputAll
+
+
+POW2 = getOutputAll(POW)
+dwPOW2 = getOutputAll(dwPOW)
+upPOW2 = getOutputAll(upPOW)
+AMC2 = getOutputAll(AMC)
+MG52 = getOutputAll(MG5)
+POHP2 = getOutputAll(POHP)
+
+for key in sorted(POW2.keys()):
+  printARow2( POW2[key], POW2["nom"])
+for key in sorted(dwPOW2.keys()):
+  printARow2( dwPOW2[key], POW2["nom"])
+for key in sorted(upPOW2.keys()):
+  printARow2( upPOW2[key], POW2["nom"])
+for key in sorted(AMC2.keys()):
+  printARow2( AMC2[key], POW2["nom"])
+for key in sorted(MG52.keys()):
+  printARow2( MG52[key], POW2["nom"])
+for key in sorted(POHP2.keys()):
+  printARow2( POHP2[key], POW2["nom"])
+
+
+
+
+
+
+
+
+
+
+
---
+++ roofit/genstudy2/test2.py
@@ -0,0 +1,175 @@
+from ROOT import *
+
+#from q2Weight_Run  import POW2, dwPOW2, upPOW2, AMC2, MG52, POHP2 
+
+def addLegendLumi():#lumi):
+  #lumi2 = str(round(lumi/100)/10)
+  title  = TLatex(-20.,50.,"2.3 fb^{-1} (13 TeV)")
+  title.SetNDC(),        title.SetTextAlign(12)
+  title.SetX(0.67),      title.SetY(0.94)
+  title.SetTextFont(42), title.SetTextSize(0.059)#,  title.SetTextSizePixels(24)
+  #title.SetTextFont(42), title.SetTextSize(0.1),  title.SetTextSizePixels(24)
+  title.Draw()
+  return title
+
+def addLegendPreliminary():
+  #tex2 = TLatex(0.3715952,0.9146667,"Preliminary")
+  tex2 = TLatex(-20.,50.,"#bf{CMS} Preliminary")
+  tex2.SetNDC(),          tex2.SetTextAlign(12)
+  tex2.SetX(0.20),        tex2.SetY(0.94)
+  #tex2.SetTextColor(2),
+  tex2.SetTextSizePixels(34)
+  tex2.SetTextFont(42) #,   tex2.SetTextSize(0.2), tex2.SetTextSizePixels(24)
+  #tex2.SetTextColor(2),   tex2.SetTextFont(42),   tex2.SetTextSize(0.05), tex2.SetTextSizePixels(24)
+  return tex2
+
+
+def addText(text,x,y):
+  title  = TLatex(-20.,50.,text)
+  title.SetNDC(),        title.SetTextAlign(12)
+  title.SetX(x),      title.SetY(y)
+  title.SetTextFont(42)#, title.SetTextSize(0.05),  title.SetTextSizePixels(24)
+  #title.SetTextFont(42), title.SetTextSize(0.1),  title.SetTextSizePixels(24)
+  return title
+
+def getEtch(cen,error,y,ey):
+  line = TLine(cen-error,y-ey,cen-error,y+ey)
+  ci = TColor.GetColor("#FF0000")
+  line.SetLineColor(ci)
+  line.SetLineWidth(2)
+  #line.SetLineStyle(2)
+  line2 = TLine(cen+error,y-ey,cen+error,y+ey)
+  line2.SetLineColor(ci)
+  line2.SetLineWidth(2)
+  #line2.SetLineStyle(2)
+
+  return line,line2
+
+def drawCXWithError(cen,err,yc):
+  x=[cen]
+  y=[yc]
+  ex=[err]
+  ey=[0.0]
+
+  from array import array
+  xx = array("d", x)
+  exx = array("d", ex)
+  yy = array("d", y)
+  eyy = array("d", ey)
+
+  gr =TGraphErrors(len(xx),xx,yy,exx,eyy)
+  gr.SetLineColor(kRed), gr.SetLineWidth(2)
+  gr.SetMarkerColor(kRed), gr.SetMarkerStyle(20)
+  #gr.Draw("LP")
+  l1,l2 =getEtch(x[0],ex[0],y[0],0.025)
+  #l1.Draw(), l2.Draw()
+  return gr,l1,l2
+
+def drawSMbar(cen,err):
+  line = TLine(cen,0,cen,1)
+  ci = TColor.GetColor("#000000")
+  line.SetLineColor(ci)
+  line.SetLineWidth(2)
+  line.SetLineStyle(2)
+  box = TBox(cen-err,0,cen+err,1)
+  ci = TColor.GetColor("#d0efd0")
+  box.SetFillColor(ci)
+  #box.Draw()
+  #line.Draw()
+  return box,line
+
+
+gROOT.SetStyle("Plain")
+gStyle.SetOptFit(1000)
+gStyle.SetOptStat("emruo")
+gStyle.SetOptStat(kFALSE)
+
+gROOT.ProcessLine(".L tdrStyle.C")
+setTDRStyle()
+gStyle.SetCanvasDefH(600)
+gStyle.SetCanvasDefW(800)
+gStyle.SetPadTickX(0)
+gStyle.SetPadTickY(0)
+
+
+gStyle.SetPadTopMargin(0.09);
+gStyle.SetPadLeftMargin(0.05);
+gStyle.SetPadRightMargin(0.05);
+
+
+c1 = TCanvas()
+
+h1 = TH1D("cx","",50,0,600)
+#h1 = TH1D("cx","",50,0,10)
+#h1 = TH1D("cx","",50,0,0.08)
+h1.GetYaxis().SetLabelSize(0)
+h1.GetYaxis().SetNdivisions(1)
+h1.GetXaxis().SetTitle("#sigma [pb] or ratio ")
+h1.Draw()
+
+pt1,pt2=addLegendLumi(),addLegendPreliminary()
+pt1.Draw(), pt2.Draw()
+
+##########################
+##########################
+##########################
+ttjjFSsim = {"cen":257, "err":26 }
+#from math import sqrt
+#ttjjFSsim = {"cen":POW2["nom"]["ttjjFSCX"], "err": sqrt(POW2["nom"]["statAcc_ttjj"]**2+ POW2["nom"]["statEff_ttjj"]**2)*POW2["nom"]["ttjjFSCX"] }
+ttjjFSfit = {"cen":176, "stat":5, "syst":33 }
+
+box,line=drawSMbar(ttjjFSsim["cen"],ttjjFSsim["err"])
+box.Draw(), line.Draw()
+
+gr,l1,l2=drawCXWithError(ttjjFSfit["cen"],ttjjFSfit["stat"],0.8)
+gr2,l3,l4=drawCXWithError(ttjjFSfit["cen"],ttjjFSfit["syst"],0.8)
+gr.Draw("LP"),l1.Draw(),l2.Draw()
+gr2.Draw("LP"),l3.Draw(),l4.Draw()
+
+
+ttjjFS=addText("t#bar{t}jj_{full phase space}",0.1,0.8)
+ttjjFS.Draw()
+ttjjFS2=addText("176 #pm 5 #pm 33 pb",0.55,0.8)
+ttjjFS2.Draw()
+
+"""
+##########################
+##########################
+##########################
+ttbbFSsim = {"cen":3.17, "err":0.41 }
+ttbbFSfit = {"cen":3.86, "stat":0.55, "syst":1.32 }
+
+boxBB,lineBB=drawSMbar(ttbbFSsim["cen"],ttbbFSsim["err"])
+boxBB.Draw(), lineBB.Draw()
+
+grBB,l1BB,l2BB=drawCXWithError(ttbbFSfit["cen"],ttbbFSfit["stat"],0.5)
+gr2BB,l3BB,l4BB=drawCXWithError(ttbbFSfit["cen"],ttbbFSfit["syst"],0.5)
+grBB.Draw("LP"),l1BB.Draw(),l2BB.Draw()
+gr2BB.Draw("LP"),l3BB.Draw(),l4BB.Draw()
+
+ttbbFS=addText("t#bar{t}b#bar{b}_{full phase space}",0.1,0.55)
+ttbbFS.Draw()
+ttbbFS2=addText("3.86 #pm 0.55 #pm 1.32 pb",0.55,0.55)
+ttbbFS2.Draw()
+
+
+##########################
+##########################
+##########################
+rFSsim = {"cen":0.012, "err":0.0001 }
+rFSfit = {"cen":0.022, "stat":0.003, "syst":0.006 }
+
+boxR,lineR=drawSMbar(rFSsim["cen"],rFSsim["err"])
+boxR.Draw(), lineR.Draw()
+
+grR,l1R,l2R=drawCXWithError(rFSfit["cen"],rFSfit["stat"],0.25)
+gr2R,l3R,l4R=drawCXWithError(rFSfit["cen"],rFSfit["syst"],0.25)
+grR.Draw("LP"),l1R.Draw(),l2R.Draw()
+gr2R.Draw("LP"),l3R.Draw(),l4R.Draw()
+
+rFS=addText("#frac{t#bar{t}b#bar{b}}{t#bar{t}jj}_{full phase space}",0.1,0.30)
+rFS.Draw()
+rFS2=addText("0.022 #pm 0.003 #pm 0.006 pb",0.55,0.30)
+rFS2.Draw()
+"""
+
