92b2d50a5a744c744e074c1a509787efb26b5847
YoungKwonJo <cccpy98@gmail.com>
2016-07-14 23:34:56 +0200

    add it

---
+++ hist2plot/sysRun.sh
@@ -0,0 +1,47 @@
+python  makeCanvasPASNormSys.py S6 30 PW pileup -b -q 
+python  makeCanvasPASNormSys.py S6 31 PW pileup -b -q
+
+python  makeCanvasPASNormSys.py S6 30 JES        JES -b -q       
+python  makeCanvasPASNormSys.py S6 31 JES        JES -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 JER        JER -b -q
+python  makeCanvasPASNormSys.py S6 31 JER        JER -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 HF         HF -b -q
+python  makeCanvasPASNormSys.py S6 31 HF         HF -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 LF         LF -b -q
+python  makeCanvasPASNormSys.py S6 31 LF         LF -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 HF_Stats1  HF_Stats1 -b -q
+python  makeCanvasPASNormSys.py S6 31 HF_Stats1  HF_Stats1 -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 HF_Stats2  HF_Stats2 -b -q
+python  makeCanvasPASNormSys.py S6 31 HF_Stats2  HF_Stats2 -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 LF_Stats1  LF_Stats1 -b -q
+python  makeCanvasPASNormSys.py S6 31 LF_Stats1  LF_Stats1 -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 LF_Stats2  LF_Stats2 -b -q
+python  makeCanvasPASNormSys.py S6 31 LF_Stats2  LF_Stats2 -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 CQ_Err1    CQ_Err1 -b -q
+python  makeCanvasPASNormSys.py S6 31 CQ_Err1    CQ_Err1 -b -q
+                                                          
+python  makeCanvasPASNormSys.py S6 30 CQ_Err2    CQ_Err2 -b -q
+python  makeCanvasPASNormSys.py S6 31 CQ_Err2    CQ_Err2 -b -q
+
+python  makeCanvasPASNormSys.py S6 30 El Electron -b -q
+python  makeCanvasPASNormSys.py S6 31 El Electron -b -q
+
+python  makeCanvasPASNormSys.py S6 30 Mu Muon -b -q
+python  makeCanvasPASNormSys.py S6 31 Mu Muon -b -q
+
+python  makeCanvasPASNormSys.py S6 30 Trig Trigger -b -q
+python  makeCanvasPASNormSys.py S6 31 Trig Trigger -b -q
+
+
+
+
+
+
