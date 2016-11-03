c167f88d799075417614d42f7a72dd6738e03483
YoungKwonJo <cccpy98@gmail.com>
2016-11-03 17:36:39 +0100

    add it

--- roofit/genstudy2/aaaaa.py
+++ roofit/genstudy2/aaaaa.py
@@ -1,6 +1,6 @@
 from bbbbb import POW,dwPOW,upPOW,AMC,MG5,POHP
 
-def printAcc(aaa):
+def printAcc(aaa,ss):
   a=round(aaa["ttbbAcc"]     *10000.)/100.
   b=round(aaa["sysAcc_ttbb"] *10000.)/100.
   c=round(aaa["ttjjAcc"]     *10000.)/100.
@@ -9,7 +9,7 @@ def printAcc(aaa):
   bbb= ss+"     & "+str(a)+"  & "+str(b)+"      & "+str(c)+" & "+str(d)+" \\\\"
   return b,d,bbb
 
-def printEff(aaa):
+def printEff(aaa,ss):
   a=round(aaa["ttbbEff"]     *10000.)/100.
   b=round(aaa["sysEff_ttbb"] *10000.)/100.
   c=round(aaa["ttjjEff"]     *10000.)/100.
@@ -20,44 +20,65 @@ def printEff(aaa):
 
 
 
-print "sample & Acc._{\\ttbb} [\\%] & Sys_{\\ttbb}^{Acc.} [\\%] &  Acc._{\\ttjj} [\\%] & Sys_{\\ttjj}^{Acc.} [\\%] \\\\"
+print "sample & $Acc._{\\ttbb}$ [\\%] & $Sys_{\\ttbb}^{Acc.}$ [\\%] &  $Acc._{\\ttjj}$ [\\%] & $Sys_{\\ttjj}^{Acc.}$ [\\%] \\\\"
+print "\\hline"
 bb,dd=0.,0.
-for ss in ['Q2_Dw3', 'nom', 'Q2_Dw1', 'Q2_Dw2', 'Q2_Up3', 'Q2_Up2', 'Q2_Up1']:
-  b,d,bbb=printAcc(POW[ss])
-  print bbb
+for ss in ['nom','Q2_Dw3', 'Q2_Dw2', 'Q2_Dw1', 'Q2_Up3', 'Q2_Up2', 'Q2_Up1']:
+  b,d,bbb=printAcc(POW[ss],ss)
+  #print bbb
+  print bbb.replace("_","\\_")
   if abs(b)>bb: bb=abs(b)
   if abs(d)>dd: dd=abs(d)
 
+print "\\hline"
+print "Sys        &       & "+str(bb)+"   &      & "+str(dd)+" \\\\"
+print "\\hline\\hline"
 
-print bb
-print dd
 
-b_dw,d_dw,dw_bbb=printAcc(dwPOW['nom'])
+b_dw,d_dw,dw_bbb=printAcc(dwPOW['nom'],'nom')
 print dw_bbb
 
-b_up,d_up,up_bbb=printAcc(upPOW['nom'])
+b_up,d_up,up_bbb=printAcc(upPOW['nom'],'nom')
 print up_bbb
 
+b_sys,d_sys=0.,0.
+if abs(b_dw)>abs(b_up): b_sys=abs(b_dw)
+if abs(b_up)>abs(b_up): b_sys=abs(b_up)
+
+print "\\hline"
+print "Sys        &       & "+str(b_sys)+"   &      & "+str(d_sys)+" \\\\"
+print "\\hline\\hline"
+    
+
 print ""
 print ""
 
-print "sample & Eff._{\\ttbb} [\\%] & Sys_{\\ttbb}^{Eff.} [\\%] &  Eff._{\\ttjj} [\\%] & Sys_{\\ttjj}^{Eff.} [\\%] \\\\"
+print "sample & $Eff._{\\ttbb}$ [\\%] & $Sys_{\\ttbb}^{Eff.}$ [\\%] &  $Eff._{\\ttjj}$ [\\%] & $Sys_{\\ttjj}^{Eff.}$ [\\%] \\\\"
 bbef,ddef=0.,0.
-for ss in ['Q2_Dw3', 'nom', 'Q2_Dw1', 'Q2_Dw2', 'Q2_Up3', 'Q2_Up2', 'Q2_Up1']:
-  b,d,bbb=printEff(POW[ss])
-  print bbb
+for ss in ['nom','Q2_Dw3', 'Q2_Dw2', 'Q2_Dw1', 'Q2_Up3', 'Q2_Up2', 'Q2_Up1']:
+  b,d,bbb=printEff(POW[ss],ss)
+  print bbb.replace("_","\\_")
   if abs(b)>bbef: bbef=abs(b)
   if abs(d)>ddef: ddef=abs(d)
 
+print "\\hline"
+print "Sys        &       & "+str(bbef)+"   &      & "+str(ddef)+" \\\\"
+print "\\hline\\hline"
 
-print bbef
-print ddef
-
-b_dwef,d_dwef,dw_bbbef=printEff(dwPOW['nom'])
+b_dwef,d_dwef,dw_bbbef=printEff(dwPOW['nom'],'nom')
 print dw_bbbef
 
-b_upef,d_upef,up_bbbef=printEff(upPOW['nom'])
+b_upef,d_upef,up_bbbef=printEff(upPOW['nom'],'nom')
 print up_bbbef
+
+bef_sys,def_sys=0.,0.
+if abs(b_dwef)>abs(b_upef): bef_sys=abs(b_dwef)
+if abs(b_upef)>abs(b_upef): bef_sys=abs(b_upef)
+
+
+print "\\hline"
+print "Sys        &       & "+str(bef_sys)+"   &      & "+str(def_sys)+" \\\\"
+print "\\hline\\hline"
 
 
 
